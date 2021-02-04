bb72f556e39377a5e88abe916fe1462170b3a267
Kathan Patel <kathanpatel111@gmail.com>
2021-02-04 09:49:56 +0530

    first commit

---
+++ lib/helper/data.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_news/models/category_model.dart';
+
+
+List<CategoryModel> getCategories(){
+
+  List<CategoryModel> category = new List<CategoryModel>();
+  CategoryModel categoryModel = new CategoryModel();
+
+  //1
+  categoryModel.categoryName = "Business";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1491336477066-31156b5e4f35?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+
+  //2
+  categoryModel.categoryName = "Entertainment";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1497562165685-9f5f905b1b96?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+  category.add(categoryModel);
+  
+  categoryModel = new CategoryModel();
+
+  //3
+  categoryModel.categoryName = "General";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1455849318743-b2233052fcff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+  category.add(categoryModel);
+  
+  categoryModel = new CategoryModel();
+
+  //4
+  categoryModel.categoryName = "Health";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+  category.add(categoryModel);
+  
+  categoryModel = new CategoryModel();
+
+   //5
+  categoryModel.categoryName = "Technology";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80";
+  category.add(categoryModel);
+  
+  categoryModel = new CategoryModel();
+
+  return category;
+
+    
+}
